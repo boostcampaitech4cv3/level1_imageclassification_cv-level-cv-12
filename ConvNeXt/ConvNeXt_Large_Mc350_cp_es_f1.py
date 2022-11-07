cdcbbc66e2e982ae2cf72dc272b68b35f5bca531
Sangjoon Han <jphan32@me.com>
2022-11-07 17:09:41 +0900

    Sangjoon Han

---
+++ ConvNeXt/ConvNeXt_Large_Mc350_cp_es_f1.py
@@ -0,0 +1,496 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch, gc
+gc.collect()
+torch.cuda.empty_cache()
+
+
+# In[2]:
+
+
+import GPUtil
+GPUtil.showUtilization()
+
+
+# In[3]:
+
+
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
+import torch_optimizer as optim
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from PIL import Image
+
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import random
+import os
+from torchmetrics import F1Score
+
+
+# In[4]:
+
+
+random_seed = 12
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+
+# In[5]:
+
+
+train_dir_path = '../input/data/train/'
+train_image_path = '../input/data/train/images/'
+
+dt_train = pd.read_csv(train_dir_path+'train.csv')
+dt_train
+
+
+# In[6]:
+
+
+def get_age_range(age):
+    if age < 30:
+        return 0
+    elif 30 <= age < 60:
+        return 1
+    else:
+        return 2
+
+
+# In[7]:
+
+
+dt_train['age_range'] = dt_train['age'].apply(lambda x : get_age_range(x))
+
+
+# In[8]:
+
+
+dt_train
+
+
+# In[9]:
+
+
+train_idx, valid_idx = train_test_split(np.arange(len(dt_train)),
+                                       test_size=0.2,
+                                       shuffle=True,
+                                       stratify=dt_train['age_range'])
+
+
+# In[10]:
+
+
+train_image = []
+train_label = []
+
+for idx in train_idx:
+    path = dt_train.iloc[idx]['path']
+    for file_name in [i for i in os.listdir(train_image_path+path) if i[0] != '.']:
+        _, file_extension = os.path.splitext(file_name)
+        if file_extension not in ['.jpg', '.jpeg', '.png']:
+            continue
+        train_image.append(train_image_path+path+'/'+file_name)
+        train_label.append((path.split('_')[1], path.split('_')[3], file_name.split('.')[0]))                                 
+
+
+# In[11]:
+
+
+valid_image = []
+valid_label = []
+
+for idx in valid_idx:
+    path = dt_train.iloc[idx]['path']
+    for file_name in [i for i in os.listdir(train_image_path+path) if i[0] != '.']:
+        _, file_extension = os.path.splitext(file_name)
+        if file_extension not in ['.jpg', '.jpeg', '.png']:
+            continue
+        valid_image.append(train_image_path+path+'/'+file_name)
+        valid_label.append((path.split('_')[1], path.split('_')[3], file_name.split('.')[0]))                                 
+
+
+# In[12]:
+
+
+def onehot_enc(x):
+    def gender(i):
+        if i == 'male':
+            return 0
+        elif i == 'female':
+            return 3
+    def age(j):
+        j = int(j)
+        if j < 30:
+            return 0
+        elif j >= 30 and j < 60:
+            return 1
+        elif j >= 60:
+            return 2
+    def mask(k):
+        if k == 'normal':
+            return 12
+        elif 'incorrect' in k:
+            return 6
+        else:
+            return 0
+    return gender(x[0]) + age(x[1]) + mask(x[2])
+
+
+# In[13]:
+
+
+# sr_data = pd.Series(whole_image_path)
+# sr_label = pd.Series(whole_target_label)
+
+
+# In[14]:
+
+
+train_data = pd.Series(train_image)
+train_label = pd.Series(train_label)
+
+valid_data = pd.Series(valid_image)
+valid_label = pd.Series(valid_label)
+
+
+# In[15]:
+
+
+class Dataset_Mask(Dataset):
+    def __init__(self, data, label, encoding=True, midcrop=True, transform=None):
+        self.encoding = encoding
+        self.midcrop = midcrop
+        self.data = data
+        self.label = label
+        self.transform = transform
+
+        if encoding:
+            self.label = self.label.apply(onehot_enc)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        X = Image.open(self.data[idx])
+        X = X.crop((17, 70, 367, 420))
+        X = self.transform(X)
+        y = self.label[idx]
+        return X, y
+
+
+# In[16]:
+
+
+mask_train_set = Dataset_Mask(data=train_data, label=train_label, transform = transforms.Compose([
+                                #transforms.RandomResizedCrop((224, 224), interpolation=transforms.InterpolationMode.BICUBIC),
+                                transforms.RandomHorizontalFlip(),
+                                transforms.ColorJitter(brightness=0.3, contrast=0.3),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                            ]))
+
+
+# In[17]:
+
+
+mask_val_set = Dataset_Mask(data=valid_data, label=valid_label, transform = transforms.Compose([
+                                transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.BICUBIC),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                            ]))
+
+
+# In[18]:
+
+
+#t_image = [mask_train_set[i][1] for i in range(len(mask_train_set))]
+#v_image = [mask_val_set[i][1] for i in range(len(mask_val_set))]
+
+
+# In[19]:
+
+
+#t_df = pd.DataFrame(t_image, columns=['counts'])
+#v_df = pd.DataFrame(v_image, columns=['counts'])
+
+
+# In[20]:
+
+
+#import seaborn as sns
+
+#fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+#sns.countplot(x='counts', data=t_df, ax=axes[0])
+#axes[0].set_xlabel("train set labels")
+#sns.countplot(x='counts', data=v_df, ax=axes[1])
+#axes[1].set_xlabel("valid set labels")
+
+
+# In[21]:
+
+
+#print(f'training data size : {len(mask_train_set)}')
+#print(f'validation data size : {len(mask_val_set)}')
+
+
+# In[22]:
+
+
+batch_size = 2048
+
+train_dataloader_mask = DataLoader(dataset = mask_train_set, batch_size=batch_size, num_workers=2)
+val_dataloader_mask = DataLoader(dataset = mask_val_set, batch_size=batch_size, num_workers=2)
+
+
+# In[23]:
+
+basemodel_convnext_l = torchvision.models.convnext_large(torchvision.models.ConvNeXt_Large_Weights.IMAGENET1K_V1)
+print('필요 입력 채널  개수', basemodel_convnext_l.features[0][0].weight.shape[1])
+print('네트워크 출력 채널 개수', basemodel_convnext_l.classifier[2].weight.shape[0])
+print(basemodel_convnext_l)
+
+# In[24]:
+
+
+import math
+class_num = 18
+"""
+basemodel_convnext_l.classifier[2] = nn.Linear(in_features=1536, out_features=class_num, bias=True)
+nn.init.xavier_uniform_(basemodel_convnext_l.classifier[2].weight)
+stdv = 1. / math.sqrt(basemodel_convnext_l.classifier[2].weight.size(1))
+basemodel_convnext_l.classifier[2].bias.data.uniform_(-stdv, stdv)
+"""
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"using {device}")
+
+if device != "cpu":
+    import torch.backends.cudnn as cudnn
+    cudnn.benchmark = True
+    if torch.cuda.device_count() > 1:
+        basemodel_convnext_l = torch.nn.DataParallel(basemodel_convnext_l)
+
+# In[26]:
+
+print(">>>>>>>>>>")
+basemodel_convnext_l = torch.load('../checkpoint/basemodel_convnext_l-2/checkpoint_best.pth')
+#if "state_dict" in pretrained_dict.keys():
+#    pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+#else:
+#    pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+#basemodel_convnext_l.load_state_dict(pretrained_dict, strict=False)
+
+basemodel_convnext_l.to(device)
+
+LEARNING_RATE = 0.001
+NUM_EPOCH = 301
+
+criterion = nn.CrossEntropyLoss()
+#optimizer = torch.optim.Adam(basemodel_convnext_l.parameters(), lr=LEARNING_RATE)
+#optimizer = torch.optim.AdamW(basemodel_convnext_l.parameters(), lr=LEARNING_RATE, eps=1e-8)
+
+optimizer = optim.AdamP(basemodel_convnext_l.parameters(), lr=LEARNING_RATE, weight_decay=1e-5, nesterov=True)
+#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+# In[27]:
+
+
+np.set_printoptions(precision=3)
+n_param = 0
+for p_idx, (param_name, param) in enumerate(basemodel_convnext_l.named_parameters()):
+    if param.requires_grad:
+        if param_name.startswith('module.classifier') or param_name.startswith('classifier'):
+            param.requires_grad = True  # Train
+        else:
+            param.requires_grad = False # Freeze
+
+        param_numpy = param.detach().cpu().numpy()
+        n_param += len(param_numpy.reshape(-1))
+        print ("[%d] name:[%s] shape:[%s]."%(p_idx,param_name,param_numpy.shape))
+        print (f"    val:{(param_numpy.reshape(-1)[:5])} -- {param.requires_grad}")
+print ("Total number of parameters:[%s]."%(format(n_param,',d')))
+
+
+# In[ ]:
+
+
+best_val_acc = 0
+best_val_loss = np.inf
+patience = 10
+cur_count = 0
+
+f1 = F1Score(num_classes=class_num, average='macro').to(device)
+best_f1_score = 0
+
+for epoch in range(NUM_EPOCH):
+    basemodel_convnext_l.train()
+    loss_value = 0
+    matches = 0
+    for train_batch in tqdm(train_dataloader_mask):
+        inputs, labels = train_batch
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        
+        outs = basemodel_convnext_l(inputs)
+        preds = torch.argmax(outs, dim=-1)
+        loss = criterion(outs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        #scheduler.step()
+        
+        if epoch % 10 == 0:
+            torch.save(basemodel_convnext_l, '../checkpoint/basemodel_convnext_l-3/checkpoint_ep_%d.pth'% epoch)
+        
+        loss_value += loss.item()
+        matches += (preds == labels).sum().item()
+        
+        train_loss = loss_value / batch_size
+        train_acc = matches / batch_size
+        
+        loss_value = 0
+        matches = 0
+    print(f"epoch[{epoch}/{NUM_EPOCH}] training loss {train_loss:.3f}, training accuracy {train_acc:.3f}")
+        
+    with torch.no_grad():
+        basemodel_convnext_l.eval()
+        val_loss_items = []
+        val_acc_items = []
+        for val_batch in tqdm(val_dataloader_mask):
+            inputs, labels = val_batch
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            
+            outs = basemodel_convnext_l(inputs)
+            preds = torch.argmax(outs, dim=-1)
+            
+            loss_item = criterion(outs, labels).item()
+            acc_item = (labels==preds).sum().item()
+            val_loss_items.append(loss_item)
+            val_acc_items.append(acc_item)
+            
+        val_loss = np.sum(val_loss_items) / len(val_dataloader_mask)
+        val_acc = np.sum(val_acc_items) / len(mask_val_set)
+
+        f1_score = f1(outs, labels)
+        
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            
+        if f1_score > best_f1_score:
+            best_f1_score = f1_score
+#             cur_count = 0
+            torch.save(basemodel_convnext_l, '../checkpoint/basemodel_convnext_l-3/checkpoint_best.pth')
+#         else:
+#             cur_count += 1
+#             if cur_count >= patience:
+#                 print("Early Stopping!")
+#                 break
+            
+            
+        print(f"[val] acc : {val_acc:.3f}, loss : {val_loss:.3f}, f1 score: {f1_score:.3f}")
+        print(f"best acc : {best_val_acc:.3f}, best loss : {best_val_loss:.3f}, best f1 : {best_f1_score:.3f}")
+
+
+# In[ ]:
+
+
+print(f'Best f1 score:{best_f1_score}')
+
+
+# In[ ]:
+
+
+# meta 데이터와 이미지 경로를 불러옵니다.
+test_dir_path = '../input/data/eval/'
+test_image_path = '../input/data/eval/images/'
+
+basemodel_convnext_l = torch.load('../checkpoint/basemodel_convnext_l-3/checkpoint_best.pth')
+submission = pd.read_csv(test_dir_path+'info.csv')
+submission.head()
+
+
+# In[ ]:
+
+
+image_paths = [os.path.join(test_image_path, img_id) for img_id in submission.ImageID]
+test_image = pd.Series(image_paths)
+
+
+# In[ ]:
+
+
+class Test_Dataset(Dataset):
+    def __init__(self, midcrop=True, transform=None):
+        self.midcrop = midcrop
+        self.data = test_image
+        self.transform = transform
+        
+    def __len__(self):
+        return len(test_image)
+    
+    def __getitem__(self, idx):
+        img = Image.open(self.data[idx])
+        img = img.crop((17, 70, 367, 420))
+        img = self.transform(img)
+        
+        return img
+
+
+# In[ ]:
+
+
+dataset = Test_Dataset(transform = transforms.Compose([
+                                transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.BICUBIC),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                        ]))
+
+loader = DataLoader(
+    dataset,
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=2
+)
+
+# 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
+device = torch.device('cuda')
+model = basemodel_convnext_l.to(device)
+model.eval()
+
+# 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
+all_predictions = []
+for images in loader:
+    with torch.no_grad():
+        images = images.to(device)
+        pred = model(images)
+        pred = pred.argmax(dim=-1)
+        all_predictions.extend(pred.cpu().numpy())
+submission['ans'] = all_predictions
+
+# 제출할 파일을 저장합니다.
+submission.to_csv(os.path.join(test_dir_path, 'submission_convnext_l.csv'), index=False)
+print('test inference is done!')
+
---
+++ preprocess/eval.py
@@ -0,0 +1,276 @@
+import os
+import sys
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import random
+
+from align_faces import warp_and_crop_face, get_reference_facial_points
+
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
+import torchvision.transforms as T
+from torchvision.transforms import Resize, ToTensor, Normalize, Compose, CenterCrop, ColorJitter
+from PIL import Image
+
+### RetinaFace
+sys.path.append('RetinaFace')
+from data import cfg_re50
+from layers.functions.prior_box import PriorBox
+from utils.nms.py_cpu_nms import py_cpu_nms
+from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
+
+### Data configure
+eval_dir_path = '../input/data/eval/'
+eval_csv_path = eval_dir_path + 'info.csv' 
+
+cropped_img_path = '../input/data/eval_crop/'
+try: 
+    os.mkdir(cropped_img_path, mode = 0o755)
+except OSError as error:
+    pass
+
+### Configure
+cfg = cfg_re50
+confidence_threshold = 0.015
+top_k = 5000
+nms_threshold = 0.4
+keep_top_k = 1
+vis_thres = 0.6
+
+### Definition
+# Position
+BBoxX1 = 0 ## Left
+BBoxY1 = 1 ## Top
+BBoxX2 = 2 ## Right
+BBoxY2 = 3 ## Bottom
+# Score
+SCORE = 4
+# Landmark
+LE_X = 5
+LE_Y = 6
+RE_X = 7
+RE_Y = 8
+N_X = 9
+N_Y = 10
+LM_X = 11
+LM_Y = 12
+RM_X = 13
+RM_Y = 14
+
+
+def get_files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield os.path.join(path, file)
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+# Fix SEED
+random_seed = 12
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+
+### net and model
+device = torch.device("cuda")
+
+detector = RetinaFace(cfg=cfg, phase='test')
+detector = load_model(detector, './RetinaFace/weights/Resnet50_Final.pth', False)
+detector.eval()
+detector = detector.to(device)
+
+transform = T.Compose([
+            T.ToTensor()
+            #T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+
+basemodel_resnet34 = torch.load('../checkpoint/resnet34_with_detect/checkpoint_ep_30.pth')
+basemodel_resnet34.eval()
+basemodel_resnet34 = basemodel_resnet34.to(device)
+
+base_transform = transforms.Compose([
+                                    ToTensor(),
+                                    Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+                            ])
+
+
+default_square = True
+inner_padding_factor = 0.05
+outer_padding = (0, 0)
+output_size = (224, 224)
+
+all_predictions = []
+
+with torch.no_grad():
+    resize = 1
+
+    # Convert and Detect -- Eval (Test only)
+    eval_csv = pd.read_csv(eval_csv_path)
+    det_idx = 0
+    for i in tqdm(range(len(eval_csv))):
+        ## Load and detection
+        filepath = eval_dir_path + f"images/{eval_csv.iloc[i]['ImageID']}"
+        file_name, file_extension = os.path.splitext(filepath)
+        if os.path.isfile(file_name + '.npy') == False:
+            img = cv2.cvtColor(cv2.imread(filepath, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+            np.save(file_name + '.npy', img)
+        
+        img_raw_rgb = np.load(file_name + '.npy')
+        img_raw_bgr = cv2.cvtColor(img_raw_rgb, cv2.COLOR_BGR2RGB)
+
+        img = np.float32(img_raw_bgr)
+        img -= (104, 117, 123)
+    
+        img_height, img_width, _ = img.shape
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img = transform(img).unsqueeze(0)
+        img = img.to(device)
+        scale = scale.to(device)
+
+        loc, conf, landms = detector(img)  # Forward
+        
+        priorbox = PriorBox(cfg, image_size=(img_height, img_width))
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2]])
+        scale1 = scale1.to(device)
+        landms = landms * scale1 / resize
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, nms_threshold)
+        # keep = nms(dets, nms_threshold,force_cpu=False)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:keep_top_k, :]
+        landms = landms[:keep_top_k, :]
+
+        dets = np.concatenate((dets, landms), axis=1)
+
+        # Not Found Face
+        if len(dets) != 1:
+            print(filepath)
+            #b = [0, 384,  64, 448, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        else:
+            b = list(map(int, dets[0]))
+
+        # show image
+        #text = "{:.4f}".format(b[SCORE])
+        #img_output = img_raw.copy()
+        #cv2.rectangle(img_output, (b[BBoxX1], b[BBoxY1]), (b[BBoxX2], b[BBoxY2]), (0, 0, 255), 2)
+        #cx = b[BBoxX1]
+        #cy = b[BBoxY1] + 12
+        #cv2.putText(img_output, text, (cx, cy), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+        # landms
+        #cv2.circle(img_output, (b[LE_X], b[LE_Y]), 1, (0, 0, 255), 4)
+        #cv2.circle(img_output, (b[RE_X], b[RE_Y]), 1, (0, 255, 255), 4)
+        #cv2.circle(img_output, (b[ N_X], b[ N_Y]), 1, (255, 0, 255), 4)
+        #cv2.circle(img_output, (b[LM_X], b[LM_Y]), 1, (0, 255, 0), 4)
+        #cv2.circle(img_output, (b[RM_X], b[RM_Y]), 1, (255, 0, 0), 4)
+
+        # save image
+        #cv2.imwrite(f"{cropped_img_path}{det_idx}.jpg", img_output)
+
+        # affine
+        #crop_with = b[BBoxX2] - b[BBoxX1]
+        #crop_height = b[BBoxY2] - b[BBoxY1]
+        #center_pos = (int(crop_with / 2) + b[BBoxX1], int(crop_height / 2) + b[BBoxY1])
+        #crop_offset = max(int(crop_with / 2), int(crop_height / 2))
+
+        #cropped_img = img_affine[max(0, center_pos[1] - crop_offset):min(img_height, center_pos[1] + crop_offset),
+        #                        max(0, center_pos[0] - crop_offset):min(img_height, center_pos[0] + crop_offset)]
+        #cropped_img = cv2.resize(cropped_img, (256, 256), interpolation=cv2.INTER_CUBIC)
+
+        
+        facial5points = [[b[LE_X], b[LE_Y]],
+                         [b[RE_X], b[RE_Y]],
+                         [b[ N_X], b[ N_Y]],
+                         [b[LM_X], b[LM_Y]],
+                         [b[RM_X], b[RM_Y]]
+                        ]
+        reference_5pts = get_reference_facial_points(output_size, inner_padding_factor, outer_padding, default_square)
+        align_crop_img = warp_and_crop_face(img_raw_bgr, facial5points, reference_pts=reference_5pts, crop_size=output_size)
+
+        cv2.imwrite(f"{cropped_img_path}{det_idx}-cropped.jpg", align_crop_img)
+        det_idx += 1
+
+        ## Classification
+        align_crop_img = base_transform(align_crop_img).unsqueeze(0)
+        align_crop_img = align_crop_img.to(device)
+
+        pred = basemodel_resnet34(align_crop_img)
+        pred = pred.argmax(dim=-1)
+        all_predictions.extend(pred.cpu().numpy())
+
+eval_csv['ans'] = all_predictions
+eval_csv.to_csv(os.path.join(eval_dir_path, 'submission_detect.csv'), index=False)
+print('test inference is done!')
